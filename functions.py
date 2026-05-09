cc27a429eda5e3d6b0886e89fed6f2d66679516c
mirzayv <mirzayv919@gmail.com>
2026-05-09 14:51:25 +0900

    feat: learn function and its define and call steps

---
+++ functions.py
@@ -0,0 +1,30 @@
+''' FUNCTIONS:
+(1) DEFINE vs CALL
+(2) Parametr vs Argument
+(3) Keyword & default arguments
+(4) Scope
+'''
+
+print("==== DEFINE (parametr) vs CALL (argument) ====")
+# build in function > print() type()
+# Function - reusable block of code!
+# Instead of block {} in JAVA, Python uses indentation!
+
+
+# DEFINE - build (parametr)
+def greet(a):
+    print(f"How do you do, {a}")
+
+
+def greeting(b):
+    print("greeting is executed")
+    return f"Hi {b}"
+
+
+# CALL - execute (argument)
+# greet("Bruce")
+result1 = greet("Bruce")
+print("result1:", result1)
+
+result2 = greeting("Justin")
+print("result2:", result2)
