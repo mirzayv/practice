0df4e3aa3d5f180e5ab4d8f8ca920571965edaa7
mirzayv <mirzayv919@gmail.com>
2026-05-12 14:42:34 +0900

    feat: specific error handling system

--- objects.py
+++ objects.py
@@ -27,3 +27,49 @@ print("result1:", result1)
 
 result2 = ceil(98.7)
 print("result2:", result2)
+
+
+# print("===== Error handling system =====")
+# car_dict = dict(name="Tayota", year=2026, electric=True)
+
+# try:
+#   print("passed here")
+#  result = car_dict["origin"]
+# print("result:", result)
+# except KeyError as err:
+#    print("No origin state property found:", err)
+# pass
+# print("result:", result)
+
+
+# print("===== Error handling system =====")
+# car_dict = dict(name="Tayota", year=2026, electric=True)
+
+# try:
+#   print("passed here")
+#  result = car_dict["year"]
+#  print("result:", result)
+# except KeyError as err:
+#   print("No origin state property found:", err)
+# else:
+#    print("Executed successfully without errors")
+# finally:
+#   print("Final closing logic")
+
+
+print("===== Error handling system =====")
+car_dict = dict(name="Tayota", year=2026, electric=True)
+
+try:
+    print("passed here")
+    a = car_dict.speed
+    result = car_dict["origin"]
+    print("result:", result)
+except KeyError as err:
+    print("No origin state property found:", err)
+except AttributeError as err:
+    print("No speed found:", err)
+else:
+    print("Executed successfully without errors")
+finally:
+    print("Final closing logic")
