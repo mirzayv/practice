ac84c344d5311618c73a9b69c6e7c3bc4db69b1f
mirzayv <mirzayv919@gmail.com>
2026-05-15 23:51:41 +0900

    feat: learn tuple and list

--- iterable.py
+++ iterable.py
@@ -1,7 +1,7 @@
 print("===== Iterable objects & RANGE =====")
 # Iterable objects > string dict tuple list range map filter
 
-range_obj = range(3)
+range_obj = range(3)  # [0, 3]
 print("range_obj:", range_obj)
 
 for letter in "MIT":
